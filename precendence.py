0fb0e3c8fe11b1fba562f193a1e4014557a10661
Ege Hatırnaz <egehatirnaz@hotmail.com>
2019-11-28 19:09:25 +0300

    Namespace implementation and example outputs are added.

--- precendence.py
+++ precendence.py
@@ -3,9 +3,6 @@
 from collections import defaultdict
 
 
-# NODE EKLENICEK
-
-
 class Graph:
     def __init__(self, vertices):
         self.graph = defaultdict(list)  # dictionary containing adjacency List
@@ -31,7 +28,7 @@ class Graph:
 
     # The function to do Topological Sort. It uses recursive
     # topologicalSortUtil()
-    def topologicalSort(self):
+    def topologicalSort(self, id):
         # Mark all the vertices as not visited
         visited = [False] * self.V
         stack = []
@@ -39,26 +36,37 @@ class Graph:
         # Call the recursive helper function to store Topological
         # Sort starting from all vertices one by one
         for i in range(self.V):
-            if visited[i] == False:
-                self.topologicalSortUtil(i, visited, stack)
+            if i in self.graph[id]:
+                if visited[i] == False:
+                    self.topologicalSortUtil(i, visited, stack)
 
         # Print contents of stack
-        print(stack)
+        stack.insert(0, id)
+        return stack
 
+
+def nameconverter(namespace, output_list):
+    #for i in output_list:
+    #    print(i)
+    new_list = [namespace[i] for i in output_list]
+    return new_list
 
 def main():
     print("=== Ex. 1 ===")
     g = Graph(6)
+    namespace = ["CAIVehicle", "CPuppet", "CPipeUser", "CAIPlayer", "CAIActor", "CAIObject"]
     g.addEdge(0, 1)
     g.addEdge(1, 2)
     g.addEdge(2, 4)
     g.addEdge(3, 4)
     g.addEdge(4, 5)
 
-    g.topologicalSort()
+    print("For 'CAIVehicle' -> ", nameconverter(namespace, g.topologicalSort(namespace.index("CAIVehicle"))))
+    print("For 'CAIPlayer' -> ", nameconverter(namespace, g.topologicalSort(namespace.index("CAIPlayer"))))
     print("=== End of Ex. 1 ===\n")
 
     print("=== Ex. 2 ===")
+    namespace = ["fstream", "ifstream", "iostream", "ofstream", "istream", "ostream", "ios"]
     g = Graph(7)
     g.addEdge(0, 2)
     g.addEdge(1, 4)
@@ -68,10 +76,13 @@ def main():
     g.addEdge(4, 6)
     g.addEdge(5, 6)
 
-    g.topologicalSort()
+    print("For 'ifstream' -> ", nameconverter(namespace, g.topologicalSort(namespace.index("ifstream"))))
+    print("For 'fstream' -> ", nameconverter(namespace, g.topologicalSort(namespace.index("fstream"))))
+    print("For 'ofstream' -> ", nameconverter(namespace, g.topologicalSort(namespace.index("ofstream"))))
     print("=== End of Ex. 2 ===\n")
 
     print("=== Ex. 3 ===")
+    namespace = ["Consultant Manager", "Director", "Permanent Manager", "Consultant", "Manager", "Temporary Employee", "Permanent Employee", "Employee"]
     g = Graph(8)
     g.addEdge(0, 3)
     g.addEdge(0, 4)
@@ -83,7 +94,9 @@ def main():
     g.addEdge(6, 7)
     g.addEdge(5, 7)
 
-    g.topologicalSort()
+    print("For 'Consultant Manager' -> ", nameconverter(namespace, g.topologicalSort(namespace.index("Consultant Manager"))))
+    print("For 'Director' -> ", nameconverter(namespace, g.topologicalSort(namespace.index("Director"))))
+    print("For 'Permanent Manager' -> ", nameconverter(namespace, g.topologicalSort(namespace.index("Permanent Manager"))))
     print("=== End of Ex. 3 ===\n")
 
 
